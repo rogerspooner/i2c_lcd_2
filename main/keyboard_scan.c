1af5df09751457ac854d1f2d2b31606244ba6426
Roger Spooner <roger.i.w.spooner@gmail.com>
2023-08-24 22:26:39 +0100

    Working now. The problem was in hardware. GPIO 34, 35 don't have pull-down resistors. I added them externally and now it detects 4 keys correctly, including combinations.

--- main/keyboard_scan.c
+++ main/keyboard_scan.c
@@ -9,19 +9,7 @@ Init:
 - Set all rows and columns to be inputs.
 - Pull down resistors on the rows.
 - Initialise "previous" list of pressed keys to empty.
-Loop:
-- Copy the previous list of pressed keys.
-- Initialise list of pressed keys to be empty.
-- Scan through the columns one at a time.
-   - Make the column output HIGH.
-   - Scan through the rows one at a time.
-   -    If the row is HIGH, then the key is pressed.
-   -    Add this key to the array of pressed keys.
-- If the list of pressed keys is different from the previous list, then
-  log changes. However the USB protocol doesn't need changes, just currently
-  pressed keys.
-  We should debounce this too, e.g. measure the time at which the key last changed
-    */
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,9 +19,9 @@ Loop:
 
 #define KEYBOARD_ROWS 4
 #define KEYBOARD_COLS 4
-#define KEY_DEBOUNCE_DELAY 3 /* measured in scan iterations */
+#define KEY_DEBOUNCE_DELAY 0 /* measured in scan iterations. Set to 0 for debug */
 static gpio_num_t keyboard_col_io[KEYBOARD_ROWS] = { GPIO_NUM_25, GPIO_NUM_26, GPIO_NUM_27, GPIO_NUM_14 };
-static gpio_num_t keyboard_row_io[KEYBOARD_COLS] = { GPIO_NUM_34, GPIO_NUM_35, GPIO_NUM_32, GPIO_NUM_33 }; // some can't be outputs
+static gpio_num_t keyboard_row_io[KEYBOARD_COLS] = { GPIO_NUM_34, GPIO_NUM_35, GPIO_NUM_32, GPIO_NUM_33 }; // some can't be outputs nor have pull-down resistors
 static int key_last_changed_time[KEYBOARD_COLS][KEYBOARD_ROWS];
 static uint8_t keys_down[2][KEYBOARD_COLS][KEYBOARD_ROWS];
 static int scan_index = 0;
@@ -46,17 +34,17 @@ esp_err_t keyboard_init()
     esp_err_t err;
     bool any_err = false;
     ESP_LOGI(TAG, "keyboard_init");
-    for (int row = 0; row < KEYBOARD_ROWS; row++) {
-        gpio_reset_pin(keyboard_row_io[row]);
-        err = gpio_set_direction(keyboard_row_io[row], GPIO_MODE_INPUT);
-        if (err) any_err = true;
-        gpio_set_pull_mode(keyboard_row_io[row], GPIO_PULLDOWN_ONLY);
-    }
     for (int col = 0; col < KEYBOARD_COLS; col++) {
         gpio_reset_pin(keyboard_col_io[col]);
         err = gpio_set_direction(keyboard_col_io[col], GPIO_MODE_INPUT);
         if (err) any_err = true;
-        gpio_set_pull_mode(keyboard_col_io[col], GPIO_PULLDOWN_ONLY);
+        gpio_set_pull_mode(keyboard_col_io[col], GPIO_PULLDOWN_ONLY); // maybe don't pulldown because we're getting a short circuit inside the ESP32
+    }
+    for (int row = 0; row < KEYBOARD_ROWS; row++) {
+        gpio_reset_pin(keyboard_row_io[row]);
+        err = gpio_set_direction(keyboard_row_io[row], GPIO_MODE_INPUT);
+        if (err) any_err = true;
+        gpio_set_pull_mode(keyboard_row_io[row], GPIO_PULLDOWN_ONLY); // GPIO 34 has no pull-down capability. Install externally.
     }
      for (int col = 0; col < KEYBOARD_COLS; col++) {
         for (int row = 0; row < KEYBOARD_ROWS; row++) {
@@ -83,6 +71,19 @@ void reset_keys_down(int k)
     }
 }
 
+/* @brief Look for pressed keys on the keyboard
+- Copy the previous list of pressed keys.
+- Initialise list of pressed keys to be empty.
+- Scan through the columns one at a time.
+   - Make the column output HIGH.
+   - Scan through the rows one at a time.
+   -    If the row is HIGH, then the key is pressed.
+   -    Add this key to the array of pressed keys.
+- If the list of pressed keys is different from the previous list, then
+  log changes. However the USB protocol doesn't need changes, just currently
+  pressed keys.
+  We should debounce this too, e.g. measure the time at which the key last changed
+*/
 esp_err_t keyboard_scan()
 {
     keys_down_index ^= 1;
@@ -94,17 +95,19 @@ esp_err_t keyboard_scan()
         gpio_set_direction(keyboard_col_io[col], GPIO_MODE_OUTPUT);
         gpio_set_level(keyboard_col_io[col], 1);
         for (int row = 0; row < KEYBOARD_ROWS; row++) {
-            if (gpio_get_level(keyboard_row_io[row]) == 1 
-                && ( key_last_changed_time[col][row] < scan_index - KEY_DEBOUNCE_DELAY
-                     || key_last_changed_time[col][row] == 0)) {
+            int v = gpio_get_level(keyboard_row_io[row]);
+            if (v == 1 )
+                //&& ( key_last_changed_time[col][row] < scan_index - KEY_DEBOUNCE_DELAY
+                //     || key_last_changed_time[col][row] == 0)) {
+            {
                 n_keys_down++;
                 keys_down[keys_down_index][col][row] = 1;
                 key_last_changed_time[col][row] = scan_index;
-                ESP_LOGI(TAG, "key down at %d,%d", col, row);
+                ESP_LOGI(TAG, "key down at %d,%d, colgpio = %d, rowgpio %d ", col, row, (int)keyboard_col_io[col], (int)keyboard_row_io[row]);
             }
         }
         gpio_set_direction(keyboard_col_io[col], GPIO_MODE_INPUT);
-        gpio_set_pull_mode(keyboard_col_io[col], GPIO_PULLDOWN_ONLY);
+        gpio_set_pull_mode(keyboard_col_io[col], GPIO_PULLDOWN_ONLY); // maybe don't pulldown because we're getting a short circuit inside the ESP32
     }
     return 0;
 };
